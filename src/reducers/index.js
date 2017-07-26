13e08943514d14d6d35e9d40c877f0c2a158155e
Andrew Mikhailov <andrew.mikhailov.18.01@gmail.com>
2017-07-26 10:36:47 +0700

    Added Redux-ORM

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import suggesterReducer from './suggester/suggester.reducer';
 import deckBuilderReducer from './deckBuilder/deckBuilder.reducer';
+import { schema } from '../models/models';
 
 export const rootReducer = combineReducers({
+    entities: schema.reducer(),
     suggester: suggesterReducer,
     deckBuilder: deckBuilderReducer,
 });
