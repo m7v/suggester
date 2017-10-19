4f43e483dbb9564f263863e2ac5bf5e7887ad50b
andrew <andrew.mikhailov.18.01@gmail.com>
2017-10-19 20:47:03 +0700

    Add old navigation. Add new Loader.

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,10 +1,10 @@
 import './Loader.css';
 import React from 'react';
-import defaultCard from './assets/default-card.png';
 
 const Loader = () => (
-    <div className="Loader la-dark la-2x">
-        <div style={{backgroundImage: `url(${defaultCard})`}} />
+    <div className='Loader__root'>
+        <div />
+        <div />
     </div>
 );
 
