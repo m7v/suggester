fe526d4b0d9a626a8eb8c84056b9d7a15f4aeccc
andrew <andrew.mikhailov.18.01@gmail.com>
2017-11-14 23:59:48 +0700

    Disable devtool extension.

--- src/core/middlewares/index.js
+++ src/core/middlewares/index.js
@@ -1,22 +1,22 @@
 import { applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { ping } from './ping';
+// import { createLogger } from 'redux-logger';
+// import { ping } from './ping';
 
-let middlewares;
-if (process.env.NODE_ENV !== 'production') {
-    const { composeWithDevTools } = require('redux-devtools-extension');
-    middlewares = composeWithDevTools(
-        applyMiddleware(
-            thunk,
-            ping,
-            createLogger(),
-        ),
-    );
-} else {
-    middlewares = applyMiddleware(
-        thunk
-    );
-}
+// let middlewares;
+// if (process.env.NODE_ENV !== 'production') {
+//     const { composeWithDevTools } = require('redux-devtools-extension');
+//     middlewares = composeWithDevTools(
+//         applyMiddleware(
+//             thunk,
+//             ping,
+//             createLogger(),
+//         ),
+//     );
+// } else {
+const middlewares = applyMiddleware(
+    thunk
+);
+// }
 
 export default middlewares;
